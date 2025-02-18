89d4c54600d2179db8ab962626bd943838d3b748
alex_matviyuk <alexmatviyuk1@gmail.com>
2025-02-18 23:05:21 +0200

    hw 7 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,36 +1,8 @@
-class StringBuilder {
-  #value;
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
 
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
+textInput.addEventListener("input", (event) => {
+  const trimmedValue = event.currentTarget.value.trim();
 
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
-
-const builder = new StringBuilder(".");
-
-console.log(builder.getValue()); // "."
-
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+  output.textContent = trimmedValue === "" ? "Anonymous" : trimmedValue;
+});
